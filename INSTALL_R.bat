1f7cc11620f1da720c6527d6e076ac09e9fa78a0
Jake Egelberg <jake1egelberg@gmail.com>
2022-10-18 08:36:03 -0400

    Update INSTALL_R.bat

--- INSTALL_R.bat
+++ INSTALL_R.bat
@@ -1,1 +1,1 @@
-powershell -Command "Invoke-WebRequest http://lib.stat.cmu.edu/R/CRAN/bin/windows/base/R-4.2.1-win.exe -OutFile R-4.2.1.exe"
+powershell -Command "Invoke-WebRequest http://lib.stat.cmu.edu/R/CRAN/bin/windows/base/R-4.1.2-win.exe -OutFile R-4.1.2.exe"
